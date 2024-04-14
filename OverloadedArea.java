f85da68b0640a9e26612aab15939e85abeec9472
RajPro77 <91749661+RajPro77@users.noreply.github.com>
2024-04-14 22:52:02 +0530

    Update OverloadedArea.java

--- OverloadedArea.java
+++ OverloadedArea.java
@@ -4,8 +4,8 @@ class OverloadedArea
 double area(double a,double b,double c)
 {
 double s,area;
-s=(a+b+c)/3.0;
-area=Math.sqrt(s*s+a*s+b*s+c);
+s=(a+b+c)/2.0;
+area=Math.sqrt(s*(s-a)*(s-b)*(s-c));
 return area;
 }
 
@@ -25,24 +25,26 @@ public static void main()
 {
 Scanner in=new Scanner(System.in);
 OverloadedArea obj=new OverloadedArea();
-double a,b,c,d1,d2,ar1,ar2,ar3;
+double a,b,c,d1,d2,rem;
 int s1,s2,h;
 System.out.println("Enter the sides of triangle");
 a=in.nextDouble();
 b=in.nextDouble();
 c=in.nextDouble();
-ar1=obj.area(a,b,c);
-
-System.out.println("Enter the sides and height");
+rem=obj.area(a,b,c);
+System.out.println("Area="rem);
+  
+System.out.println("Enter the sides and height of rhombus");
 s1=in.nextInt();
 s2=in.nextInt();
 h=in.nextInt();
-ar2=obj.area(s1,s2,h);
-
+rem=obj.area(s1,s2,h);
+System.out.println("Area="rem);
+  
 System.out.println("Enter the diagonals of Paralellogram");
 d1=in.nextDouble();
 d2=in.nextDouble();
-ar3=obj.area(d1,d2);
-
+rem=obj.area(d1,d2);
+System.out.println("Area="rem);
 }
 }
